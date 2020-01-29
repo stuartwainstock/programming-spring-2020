d6e7c27f8d6477be7110e739b9bc38125a5b9d78
Stuart <stuart.wainstock@gmail.com>
2020-01-29 20:42:05 +0900

    wk2.2 Objects in Array

---
+++ wk2/2.2/js/main.js
@@ -0,0 +1,5 @@
+let favMovies = ['Vegas Vacation', 'City of God', 'Devil Wears Prada', 'Summer Catch', 'Ghost Bird'];
+
+console.log(favMovies);
+favMovies.push(prompt("What's your favorite movie?"));
+console.log(favMovies.toString());
