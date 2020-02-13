e8788a49025ede60e6ab33d291f4e62fe47650a5
Stuart <stuart.wainstock@gmail.com>
2020-02-13 13:55:34 +0900

    assignment 4.1 using DOM to add elements to HTML and event listener

--- wk4/4.1/js/dommanip.js
+++ wk4/4.1/js/dommanip.js
@@ -1,41 +1,15 @@
-let x;
-let offset = 10;
+//Use querySelector to select empty div and assign to variable
+const myEmptyDiv = document.querySelector('#myEmptyDiv');
 
-function setup() {
-	createCanvas(240, 120);
-  x = width/2;
-}
+const heading = document.createElement('h1');
 
-/*function createTile() {
-  translate(0,0);
-  fill('#00548D'); //blue
-  rect(0,0,200,200);
-  stroke('#FDBE42');
-  strokeWeight(8);
-  noFill(); 
-  rect(25,25,150,150);
-  noStroke();
-  fill('#FDBE42'); //yellow
-  arc(50, 50, 50, 50, 0, 90);
-  arc(150, 50, 50, 50, 90, 180);
-  arc(150, 150, 50, 50, 180, 270);
-  arc(50, 150, 50, 50, 270, 360);
-  fill('#F54053'); 
-  quad(50, 100, 100, 50, 150, 100, 100, 150);
-}*/
+heading.innerHTML = 'Testing creating a h1 via the innerHTML';
+heading.style.cursor = 'pointer';
+heading.addEventListener('click', handleHeadingClick);
+myEmptyDiv.appendChild(heading);
 
-function draw(){
- background(204);
- if (mouseX > x) {
-  x += 0.5;
-  offset = -10;
- }
- if (mouseX < x){
-  x -= 0.5;
-  offset = 10;
- }
- line(x, 0, x, height);
- line(mouseX, mouseY, mouseX + offset, mouseY - 10);
- line(mouseX, mouseY, mouseX + offset, mouseY + 10);
- line(mouseX, mouseY, mouseX + offset*3, mouseY);
+function handleHeadingClick (){
+  console.log('heading clicked');
+  document.body.style.backgroundColor = '#70ea45';
+  document.body.style.color = '#921abd';
 }
