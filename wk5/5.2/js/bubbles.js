5f03cc29e873468eb5e10f0ca0068e826783c1d4
Stuart <stuart.wainstock@gmail.com>
2020-02-21 11:47:52 +0900

    init bubble exercise

---
+++ wk5/5.2/js/bubbles.js
@@ -0,0 +1,34 @@
+let bubble1;
+let bubble2;
+
+class Bubble{
+	constructor(){
+		this.x = 150;
+		this.y = 100;
+	}
+	move(){
+		this.x = this.x + random(-10, 10);
+		this.y = this.y + random(-10, 10);
+	}
+	show(){
+		strokeWeight(2);
+		stroke('#004192');
+		noFill();
+		ellipse(this.x, this.y, 24, 24);
+	}
+}
+
+function setup(){
+	createCanvas(500,500);
+	bubble1 = new Bubble();
+	bubble2 = new Bubble();
+	print(bubble1.x, bubble1.y);
+}
+
+function draw(){
+	background('#d9761a');
+	bubble1.move();
+	bubble1.show();
+	bubble2.move();
+	bubble2.show();
+}
