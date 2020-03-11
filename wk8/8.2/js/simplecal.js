15aae197e00bc8471af471472bbdceb0cae84763
Stuart <stuart.wainstock@gmail.com>
2020-03-11 15:01:19 -0500

    two inputs methods

--- wk8/8.2/js/simplecal.js
+++ wk8/8.2/js/simplecal.js
@@ -1,6 +1,31 @@
-new Vue({
+const vm = new Vue({
 	el: '#simplecalculator',
 	data: {
-		
+		firstNumber: null,
+		secondNumber: null,
+		operation: 'Addition',
+		total: null
+	},
+	methods: {
+		calculate: (num1, num2) => {
+			switch (vm.operation) {
+				case 'Addition':
+					vm.total = num1 + num2;
+					break;
+				case 'Subtraction':
+					vm.total = num1 - num2;
+					break;
+				case 'Division':
+					vm.total = num1 / num2;
+					break;
+				case 'Multiplication':
+					vm.total = num1 * num2;
+					break;
+				default: 
+					window.alert('Unexpected Operator');
+					break;
+			}
+			vm.firstNumber = vm.secondNumber = null;
+		}
 	}
 });
