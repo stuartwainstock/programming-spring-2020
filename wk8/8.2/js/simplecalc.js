d6445f5876290de811f13d8bcbda3401d0805d09
Stuart <stuart.wainstock@gmail.com>
2020-03-12 15:53:44 -0500

    added v-on

--- wk8/8.2/js/simplecalc.js
+++ wk8/8.2/js/simplecalc.js
@@ -8,19 +8,19 @@ const vm = new Vue({
 	},
 	methods: {
 		calculate: (num1, num2) => {
-			switch(vm.operation){
-				case 'Addition':
-					vm.total = num1 + num2;
-					break;
-				case 'Subtraction':
-					vm.total = num1 - num2;
-					break;
-				case 'Division':
-					vm.total = num1 / num2;
-					break;
-				case 'Multiplication':
-					vm.total = num1 * num2;
-					break;
+			switch (vm.operation) {
+		        case 'Addition':
+		          vm.total = num1 + num2;
+		          break;
+		        case 'Subtraction':
+		          vm.total = num1 - num2;
+		          break;
+		        case 'Multiplication':
+		          vm.total = num1 * num2;
+		          break;
+		        case 'Division':
+		          vm.total = num1 / num2;
+		          break;
 				default: 
 					window.alert('Unexpected Operator');
 					break;
