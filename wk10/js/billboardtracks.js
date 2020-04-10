b40241109e13ee3404fe616fb5cf8c92130c13c9
Stuart <stuart.wainstock@gmail.com>
2020-04-10 13:38:02 +0900

    trending styles and computed

--- wk10/js/billboardtracks.js
+++ wk10/js/billboardtracks.js
@@ -1,10 +1,22 @@
 Vue.component('streaming-track', {
-	template: `<div class="track"> 
+	template: `<div v-bind:class="['track', trending]"> 
 		<h3> {{track.title}} </h3>
 		<div> <img :src="track.cover" alt=""> </div>
 		<small>{{track.artist}}</small>
 	</div>`,
-	props: ['track']
+	props: ['track'],
+	computed: {
+		trending: function () {
+			const delta = this.track.rank - this.track.position.positionLastWeek;
+			if (delta > 0){
+				return 'up'
+			} else if  (delta < 0 ) {
+				return 'down'
+			} else {
+				return 'no-change'
+			}
+		}
+	}
 });
 
 const vm = new Vue({
