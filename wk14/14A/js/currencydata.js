740aca549994e95bb38a547fb293afbff5574561
Stuart <stuart.wainstock@gmail.com>
2020-05-07 14:07:55 +0900

    updating language on page

--- wk14/14A/js/currencydata.js
+++ wk14/14A/js/currencydata.js
@@ -1,9 +1,8 @@
 Vue.component('currency-trend', {
 	template: `<div v-bind:class="['currency']"> 
-		<h3> {{currency.name}} </h3>
-		<p> {{currency.city}}, {{currency.state}} </p>
- 		<div> <p :src="currency.website_url">{{currency.website_url}}</p> </div>
- 		<small>Brewery Type: {{currency.brewery_type}}</small>
+		<h3> Current Rate: {{currency.rates}} </h3>
+		<p> {{currency.rates}}, {{currency.base}} </p>
+ 		<!--<small>Country of Currency: {{currency.rates.2020-01-02.USD}}</small>-->
  	</div>`,
  	props: ['currency']
 
@@ -16,7 +15,7 @@ const vm = new Vue({
 	},
 	mounted () {
 		axios
-			.get('https://api.exchangeratesapi.io/history?start_at=2020-01-01&end_at=2020-05-01')
+			.get('https://api.exchangeratesapi.io/history?start_at=2020-01-01&end_at=2020-05-01&base=USD')
 			.then(response => {
 				console.log('response', response);
 				vm.currencies = response.data;
