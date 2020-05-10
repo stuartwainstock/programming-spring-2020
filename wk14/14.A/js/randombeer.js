8100392cd4b2f7b18211070cb1014937b5b39d06
Stuart <stuart.wainstock@gmail.com>
2020-05-10 13:07:37 +0900

    added P5 and DOM

--- wk14/14.A/js/randombeer.js
+++ wk14/14.A/js/randombeer.js
@@ -25,4 +25,25 @@ const vm = new Vue({
 	}
 });
 
+var circ_size = 10;
+
+function setup() { 
+  createCanvas(200, 200);
+} 
+
+// When the circle gets bigger than the screen
+// Make it small again
+function draw() { 
+	background(255);
+  fill("red");
+  ellipse(120, 120, circ_size, circ_size);
+  circ_size += 2;
+  
+  if (circ_size > 100) {
+    circ_size = 0;
+  }
+  if (mouseIsPressed) {
+    noLoop()
+  }
+}
 
